20d866b2531224af6436923c9106a3c23578f447
alicia.brandewie@gmail.com <alicia@Alicia-PC.localdomain>
2025-07-08 15:30:52 -0500

    firstCardClicked and secondCardClicked

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 /*-------------- Constants -------------*/
-const cards =
+const CARDS =
 [0, 1,  2,  3,
  4, 5,  6,  7,
  8, 9, 10, 11,
 12, 13, 14, 15]
 
+const WINNING_PAIRS = 8;
+    
+const MAXIMUM_TRIES = 10
 
 /*---------- Variables (state) ---------*/
 let board
 
-let cardA 
-    // index value
-let cardB 
-    // index value
+let firstCardClicked
+    // undefined, so falsey
+let secondCardClicked 
+    // undefined, so falsey
 let match
     //boolean
-let pairs
-    // number, X of 8
-let tries
-    // number, 10-x remaining
+let pairsCount
+    // count from 0 to 8
+let triesCountDown
+    //count down from 10
+   // let lose = numberTries > MAXIMUM_TRIES; // is this necessary with let won?
 let won
    //boolean 
 let message
@@ -30,36 +34,73 @@ let playAgain
 /*----- Cached Element References  -----*/
 
 const cardEls = document.querySelectorAll(".card")
-console.log(cardEls)
-
+console.log(cardEls);
 
 /*-------------- Functions -------------*/
 
-const initialize = () => {
-    board = ["", "", "", "", "", "", "", "", "", "","", "", "", "", "", ""];
-    cardA = "";
-    cardB = "";
-    match = false;
-    pairs = 0;
-    tries = 10;
-    won = false;
-    message = "";
-    playAgain = false;
+// can't add event listner to array, have to loop trhough them; DOM events to loop through node list like this with for each 
+// then add event listener to that function
 
-    render();
-};
-console.log (initialize)
+cardEls.forEach((card) => {
+    card.addEventListener("click",(event) => {
+        console.log(event.target.innerText); // place holder, replace with logic 
+        // compare 
+        //add more functions, decisions to make 
+        //first card or second
+        console.log(firstCardClicked); 
+        console.log(secondCardClicked);
+        if (firstCardClicked === undefined) {
+            firstCardClicked = event.target.innerText;
+            console.log(firstCardClicked)
+        } else {
+            secondCardClicked = event.target.innerText;
+            console.log(secondCardClicked)
+        }
+        
 
-const render = () => {
-    updateBoard()
-    updateMatch()
-    updatePairs()
-    updateTries()
-    updateMessage()
-}
+        // if first card, how do store value to compare later
+        //if second card, how to compare to first card
+        //what to do if they do match
+        //what to do if they don't match 
+    });
+});
 
 
-initialize()
+
 
 /*----------- Event Listeners ----------*/
 
+
+
+
+
+
+
+/* code grave yard
+// const initialize = () => {
+//     board = ["", "", "", "", "", "", "", "", "", "","", "", "", "", "", ""];
+//     cardA = "";
+//     cardB = "";
+//     match = false;
+//     pairs = 0;
+//     tries = 10;
+//     won = false;
+//     message = "";
+//     playAgain = false;
+
+//     render();
+// };
+// console.log (initialize)
+
+
+// const render = () => {
+//     updateBoard()
+//     updateMatch()
+//     updatePairsCount()
+//     updateTriesCountDown()
+//     updateMessage()
+// }
+
+//initialize()
+
+*/
