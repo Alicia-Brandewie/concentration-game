f5c61b700e68e24a22664a110f13fea20f91a493
alicia.brandewie@gmail.com <alicia@Alicia-PC.localdomain>
2025-07-08 10:57:57 -0500

    concentration-game-backend-setup

---
+++ README.md
@@ -0,0 +1,6 @@
+# Concentation Game
+
+
+
+Attributions
+referenced "Javascript-bowswer-game-tic-tac-toe-lab" for inital html, css, and JS setup
--- app.js
+++ app.js
@@ -1,14 +1,65 @@
 /*-------------- Constants -------------*/
+const cards =
+[0, 1,  2,  3,
+ 4, 5,  6,  7,
+ 8, 9, 10, 11,
+12, 13, 14, 15]
 
 
 /*---------- Variables (state) ---------*/
+let board
+
+let cardA 
+    // index value
+let cardB 
+    // index value
+let match
+    //boolean
+let pairs
+    // number, X of 8
+let tries
+    // number, 10-x remaining
+let won
+   //boolean 
+let message
+    // string
+let playAgain
+    // boolean
 
 
 /*----- Cached Element References  -----*/
 
+const cardEls = document.querySelectorAll(".card")
+console.log(cardEls)
+
 
 /*-------------- Functions -------------*/
 
+const initialize = () => {
+    board = ["", "", "", "", "", "", "", "", "", "","", "", "", "", "", ""];
+    cardA = "";
+    cardB = "";
+    match = false;
+    pairs = 0;
+    tries = 10;
+    won = false;
+    message = "";
+    playAgain = false;
+
+    render();
+};
+console.log (initialize)
+
+const render = () => {
+    updateBoard()
+    updateMatch()
+    updatePairs()
+    updateTries()
+    updateMessage()
+}
+
+
+initialize()
 
 /*----------- Event Listeners ----------*/
 
